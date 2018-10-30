ed929364630645dc3c1667fd317aedc1a67657a9
Nicholas Gingras <itsnickg11@gmail.com>
2018-10-30 17:28:16 -0400

    Renamed method as well as added catch for mismatched input

--- src/NIM/Muliplayer.java
+++ src/NIM/Muliplayer.java
@@ -1,12 +1,22 @@
 package NIM;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Muliplayer {
         Scanner input = new Scanner(System.in);
-        public void Multi(){
+
+    public void playerCreation() {
             int numberPlayers;
             System.out.println("How many players would you like to have? ");
-            numberPlayers = input.nextInt();
+        while (true) {
+            try {
+                numberPlayers = input.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please give me an actual number.");
+            }
+        }
             int[] players;
             players = new int[numberPlayers];
         }
