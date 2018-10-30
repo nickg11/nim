222b9101aded5877b6797e9fdc826c92ac98971d
PhoenixRemington <walkertechranger@gmail.com>
2018-10-30 17:17:48 -0400

    The start

--- src/NIM/Muliplayer.java
+++ src/NIM/Muliplayer.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 public class Muliplayer {
         Scanner input = new Scanner(System.in);
         public void Multi(){
+            int numberPlayers;
             System.out.println("How many players would you like to have? ");
+            numberPlayers = input.nextInt();
+            int[] players;
+            players = new int[numberPlayers];
 
         }
 
